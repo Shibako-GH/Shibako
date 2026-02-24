84dab2e4b1f361e9fe72ce5b3ca26f9872133585
Shibako-GH <ssuyoung4655@gmail.com>
2026-02-24 15:13:28 +0900

    Add files via upload

---
+++ PrintfTwo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("My name is Shibako\n");
+    printf("My age is %d\n", 67);
+    printf("My birthday is %d.%d.%d\n", 3, 15, 2007);
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("123\n");
+    printf("1 2 3\n");
+    printf("1  2  3\n");
+    return 0;
+}
